b94ca6376149246d99c3cd68823af79a1d39c80f
shoaib memon <shoaibattari83@gmail.com>
2024-02-12 23:02:39 +0500

    Add Code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="mx-auto max-w-[750px] flex justify-center">{children}</div>
+        <div className="mx-auto max-w-[750px] p-3">{children}</div>
       </body>
     </html>
   );
